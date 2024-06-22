565017cdb31ef4eea954b158fd8c09c915423663
Pawan Kumar <87902584+thepkpawankumar@users.noreply.github.com>
2024-06-22 22:16:18 +0530

    Update AuthContext.tsx

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,23 +1,18 @@
 import React, { ReactNode, createContext, useContext, useEffect, useState } from 'react'
-
 type User = {
     name: string;
     email: string;
 }
 export const AuthContext = createContext<User | null>(null);
-
 export const useGetAuthContext = () => {
-
     let user = useContext(AuthContext);
     return {
         user
     }
 }
 export const AuthContextProvider = ({children}: {children: ReactNode}) => {
-
     const [user, setUser] = useState<User | null>(null);
     useEffect( () => {
-
         
     }, []);
   return (
