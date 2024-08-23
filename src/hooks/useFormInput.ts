747fe2968063a858712a097fa824195cc6995382
Pawan Kumar <87902584+thepkpawankumar@users.noreply.github.com>
2024-08-23 23:38:02 +0530

    Update useFormInput.ts

--- src/hooks/useFormInput.ts
+++ src/hooks/useFormInput.ts
@@ -5,7 +5,6 @@ export const useFormInput = () => {
     const [value, setValue] = useState("");
 
     const handleValueChange = (e: {target: HTMLInputElement}) => setValue(e.target.value);
-
     return {
         
         value: value,
